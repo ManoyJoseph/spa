b1e9ef9a2c7551fce2babf198a80348cbe5ec76a
Your Name <you@example.com>
2025-05-13 10:59:44 +0000

    mana

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
 Alpine.store("settings", {
-    apiBaseUrl:"http://172.17.100.14:3332/default/api/",  
+    apiBaseUrl:"http://172.17.100.14:3353/pujida4/api/",  
     appName: "Ecoomerce store",  
 });
